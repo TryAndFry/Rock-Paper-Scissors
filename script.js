ce4e3e94163a44aafca7eaaac2bb17b4d102d1e1
Ryan Baten <rbaten751991@gmail.com>
2021-11-19 19:35:02 -0500

    add gui and adust JS accordingly

--- script.js
+++ script.js
@@ -1,17 +1,82 @@
+const difficultyButtons=document.documentElement.querySelectorAll('.difficulty-setting');
+const gameButtons=document.documentElement.querySelectorAll('.game-button');
+const gameRecord=document.documentElement.querySelector('.game-record');
+const gameResult=document.documentElement.querySelector('.result')
+let difficulty=1;
+let wins=0;
+let losses=0;
+let playerSelection='';
+let computerSelection=''
+
+function setDifficulty(){
+    difficultyButtons.forEach(button =>{
+        button.classList.remove('selected')
+    })
+    this.classList.add('selected')
+    difficulty=parseInt(this.value);
+}
+
+
+function playGame(){
+    gameButtons.forEach(button =>{
+        button.classList.remove('computer-selection')
+    })
+    playerSelection=this.id;
+    switch (difficulty){ // easy mode, player always wins
+            case 1:
+                {switch (playerSelection){
+                    case 'rock': 
+                        computerSelection='scissors';
+                        break;
+                    case 'paper': 
+                        computerSelection='rock';
+                        break;
+                    case 'scissors': 
+                        computerSelection='paper';
+                        break;
+            }}
+                break;
+            case 2: //randomized
+                computerSelection=computerPlay();
+                break;
+            case 3: // hard mode, player always loses
+                {switch (playerSelection){
+                    case 'rock': 
+                        computerSelection='paper';
+                        break;
+                    case 'paper': 
+                        computerSelection='scissors';
+                        break;
+                    case 'scissors':
+                        computerSelection='rock';
+                        break;
+            }}
+                break;
+    }
+    document.documentElement.querySelector(`#${computerSelection}`).classList.add('computer-selection')
+    let [text, result] = (playRound(computerSelection,playerSelection));
+    result === 1 ? wins++ : '';
+    result === -1 ? losses++ : '';
+    gameRecord.innerText=`Wins: ${wins} Losses: ${losses}`;
+    gameResult.innerText=`${text}`
+}
+
+
+difficultyButtons.forEach(button =>{
+    button.addEventListener('click',setDifficulty)
+})
+
+gameButtons.forEach(button =>{
+    button.addEventListener('click',playGame)
+})
+
 function computerPlay(){
     let selection=Math.floor(Math.random()*3);
     if(selection==0) return "rock";
     if(selection==1) return "paper";
     if(selection==2) return "scissors";
 }
-function getPlayerSelection(){
-    let playerSelection=prompt("Please select rock, paper, or scissors");
-    playerSelection=playerSelection.trim().toLowerCase();
-    if(!(playerSelection=='rock' || playerSelection=='paper' || playerSelection=='scissors')){
-        return -1;
-    }
-    return playerSelection;
-}
+
 function playRound(computerPlay,playerSelection){
     if(computerPlay==playerSelection){
         return ["Draw! You both chose " + playerSelection +"!",0]
@@ -42,23 +107,8 @@ function playRound(computerPlay,playerSelection){
     }
 }
 
-function game(){
-    let i=0;
-    let numWins=0;
-    while(i<=4){
-        let playerSelection=getPlayerSelection();
-            if(playerSelection==-1){
-                alert('Not a valid entry. Game is void')
-                return;
-            }
-        let computerSelection=computerPlay();
-        let result=playRound(computerSelection,playerSelection)
-        numWins+=result[1];
-        //console.log(result[0],numWins)
-        i++;
-    }
-    if(numWins>0) console.log('You Win best of 5!')
-    if(numWins==0) console.log('You Draw!')
-    if(numWins<0) console.log('You Lose best of 5!')
-}
-game();
+document.body.addEventListener('mousemove',() =>{
+    gameButtons.forEach(button =>{
+        button.classList.remove('computer-selection')
+    })
+})
